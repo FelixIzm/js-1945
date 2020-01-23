de05997529e125dabc2465f0dd4b0650b61c39f5
FelixIzm <for_cv@mail.ru>
2020-01-23 22:58:35 +0200

    midhight

--- src/test1.js
+++ src/test1.js
@@ -6,6 +6,11 @@ const str_00 = 'bda88568a54f922fcdfc6dbf940e5d00';
 const str_0b = '56105c9ab348522591eea18fbe4d080b';
 const str_PNSESSIONID = 'PNSESSIONID';
 var cook_00 = cook_0b = cook_PNSESSIONID = '';
+military_unit = '147 сд';
+start_date = '1945-5-1';
+finish_date = '1945-5-31';
+size_rec = 10;
+from_rec = 0;
 
 function dict2str(dict){
 	_str = '';
@@ -32,23 +37,62 @@ const options_01 = {
 function failureCallback(error) {
   console.log("Завершено с ошибкой " + error);
 }
+function callback_result(options){
+  console.log(options);
+  options['method'] = 'POST';
+  
+  data = JSON.parse(body);
+  total = data['hits']['total']; 
+  hits = data['hits']['hits'];
+  //console.log(hits[0]['_source']);
+  for (i in hits){
+    //console.log(hits[i]['_source']);
+  }
+  console.info(' total = '+data['hits']['total']);
+}
 
 function request_03(options) {
     return new Promise((resolve, reject) => {
-        console.log(options);
+      
+      var _cookie = {};
         request(options, function (error, response, body) {
-
+          if(error){
+              reject({});
+            }
+            if (response) {
+            /*******************************************/
+            /*******  Готовим ЧЕТВЕРТЫЙ ЗАПРОС *********/
+            /*******************************************/
+            headers=parse_file('./mu_files/mu_header4.txt')
+            headers['Content-Type'] = 'application/json'
+            headers['Origin']='https://pamyat-naroda.ru'
+            headers['Referer']='https://pamyat-naroda.ru/documents/'
+            bs = options['cookies'][str_00];
+            bs += "=" * ((4 - bs.length % 4) % 4);
+            console.log('bs = '+bs);
+            bs = Buffer.from(bs, 'base64').toString('ascii');
+            a_bs = bs.split('XXXXXX')[0];
+            b_bs = bs.split('XXXXXX')[1].split('YYYYYY')[0];
+            //console.log(bs);
+            //console.log('a_bs = '+a_bs);
+            //console.log('b_bs = '+b_bs);
+            data_t = `{"query":{"bool":{"should":[{"bool":{"should":[{"match_phrase":{"document_type":"Боевые донесения, оперсводки"}},{"match_phrase":{"document_type":"Боевые приказы и распоряжения"}},{"match_phrase":{"document_type":"Отчеты о боевых действиях"}},{"match_phrase":{"document_type":"Переговоры"}},{"match_phrase":{"document_type":"Журналы боевых действий"}},{"match_phrase":{"document_type":"Директивы и указания"}},{"match_phrase":{"document_type":"Приказы"}},{"match_phrase":{"document_type":"Постановления"}},{"match_phrase":{"document_type":"Доклады"}},{"match_phrase":{"document_type":"Рапорты"}},{"match_phrase":{"document_type":"Разведывательные бюллетени и донесения"}},{"match_phrase":{"document_type":"Сведения"}},{"match_phrase":{"document_type":"Планы"}},{"match_phrase":{"document_type":"Планы операций"}},{"match_phrase":{"document_type":"Карты"}},{"match_phrase":{"document_type":"Схемы"}},{"match_phrase":{"document_type":"Справки"}},{"match_phrase":{"document_type":"Прочие документы"}}]}},{"bool":{"should":[{"bool":{"must":[{"range":{"date_from":{"lte":"${finish_date}"}}},{"range":{"date_to":{"gte":"${start_date}"}}}],"boost":3}},{"bool":{"must":[{"range":{"document_date_b":{"lte":"${finish_date}"}}},{"range":{"document_date_f":{"gte":"${start_date}"}}}],"boost":7}}]}},{"bool":{"should":[{"match_phrase":{"authors_list.keyword":{"query":"${military_unit}","boost":50}}},{"match":{"document_name":{"query":"${military_unit}","type":"phrase","boost":30}}},{"match":{"authors":{"query":"${military_unit}","type":"phrase","boost":20}}},{"match":{"army_unit_label.division":{"query":"${military_unit}","type":"phrase","boost":10}}},{"nested":{"path":"page_magazine","query":{"bool":{"must":[{"match":{"page_magazine.podrs":{"query":"${military_unit}","type":"phrase"}}},{"range":{"page_magazine.date_from":{"lte":"${finish_date}"}}},{"range":{"page_magazine.date_to":{"gte":"${start_date}"}}}]}},"boost":4}}]}}],"minimum_should_match":3}},"_source":["id","document_type","document_number","document_date_b","document_date_f","document_name","archive","fond","opis","delo","date_from","date_to","authors","geo_names","operation_name","secr","image_path","delo_id","deal_type","operation_name"],"size":"${size_rec}","from":"${from_rec}"}`;
+            url4 = 'https://cdn.pamyat-naroda.ru/data/'+a_bs+'/'+b_bs+'/pamyat/document,map,magazine/_search';
+            options['url']=url4;
+            options['headers']=headers;
+            options['form'] =  data_t;
+            resolve(options);
+          }
         })
-    })
-
-  }
+    });
+}
 
 
 function request_02(options) {
   return new Promise((resolve, reject) => {
     //console.log(options);
+    var _cookie = {};
     request(options, function (error, response, body) {
-        var _cookie = {};
         if(error){
             reject({});
         }
@@ -74,11 +118,11 @@ function request_02(options) {
             resolve(options);
         }
     })
-  })
+  });
 }
 
 function request_01() {
-  return new Promise((resolve, reject) => {
+  return  new Promise((resolve, reject) => {
         var _cookie = {};
         request(options_01, function (error, response, body) {
             if(error){
@@ -112,9 +156,11 @@ function request_01() {
             }
     });
 
-  })
+  });
 }
 
 request_01()
     .then(request_02)
-    .then(request_03);
+    .then(request_03)
+    .then(callback_result);
+
